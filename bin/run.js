f5276dfe60bec698bbca8d83835bda9778cc966d
ganeshpatki <ganeshpatki5371@gmail.com>
2016-11-21 19:27:27 -0500

    initial commit

---
+++ bin/run.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const service = require("../server/service");
+const http = require("http");
+
+const server = http.createServer(service);
+server.listen(3000);
+
+server.on('listening', function(){
+    console.log(`Slack is listening on ${server.address().port} in ${service.get('env')} mode`);
+
+});
